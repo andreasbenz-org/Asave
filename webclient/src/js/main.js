42e02dc5777412eee5f3e7802bbfaa5ec7a2b5f8
Growing Dev <54740321+GrowingDev@users.noreply.github.com>
2022-08-29 19:05:51 +0200

    add layout for landingpage

--- webclient/src/js/main.js
+++ webclient/src/js/main.js
@@ -1,5 +1,19 @@
 // Import our custom CSS
 import '../scss/styles.scss'
 
+import AsaveLogo from '../assets/imgs/app-logo.png'
+import Certificate from '../assets/imgs/certificate.png'
+
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/regular'
+import '@fortawesome/fontawesome-free/js/brands'
+
+const navbarLogoImg = document.getElementById('navbar-logo');
+navbarLogoImg.src = AsaveLogo;
+
+const footerCertImg = document.getElementById('footer-certificate');
+footerCertImg.src = Certificate;
+
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
--- webclient/webpack.config.js
+++ webclient/webpack.config.js
@@ -1,10 +1,12 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/js/main.js',
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    assetModuleFilename: '[name][ext]'
   },
   devServer: {
     static: path.resolve(__dirname, 'dist'),
@@ -36,7 +38,18 @@ module.exports = {
             loader: 'sass-loader'
           }
         ]
-      }
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Asave Security Service GmbH',
+      filename: 'index.html',
+      template: 'src/templates/index.html'
+    })
+  ]
 }
